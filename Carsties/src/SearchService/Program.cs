cf2bdfb5b141125cafd916fa8608ed7eed819f59
Usman <usmanmalik9797@gmail.com>
2025-05-27 18:12:16 -0500

    .

---
+++ Carsties/src/SearchService/Program.cs
@@ -0,0 +1,29 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings
+.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Item>()
+.Key(x => x.Make, KeyType.Text)
+.Key(x => x.Model,KeyType.Text)
+.Key(x => x.Color, KeyType.Text)
+.CreateAsync();
+
+app.Run();
